2075396ec98960d41c05408f9b632ee4f2593c66
NeoDalmatian <aviskovic100@gmail.com>
2022-07-25 22:10:52 +0200

    implement functionality for CANVAS SIZE button

--- js/app.js
+++ js/app.js
@@ -1,10 +1,25 @@
 const grid = document.querySelector(".grid");
 const button = document.querySelector("button");
 
-for (let i = 0; i < 256; i++) {
-  const pixel = document.createElement("div");
-  pixel.addEventListener("mousemove", (e) => {
-    e.target.setAttribute("style", "background: black;")
-  })
-  grid.appendChild(pixel);
+let canvasSize = 256;
+let n = 16;
+
+button.addEventListener("click", () => {
+  n = parseInt(prompt("Set canvas size (16-100)"));
+  grid.setAttribute("style", `grid-template-columns: repeat(${n}, 1fr);`)
+  canvasSize = (n * n);
+  while (grid.firstChild) {grid.firstChild.remove()};
+  drawCanvas();
+})
+
+function drawCanvas() {
+  for (let i = 0; i < canvasSize; i++) {
+    const pixel = document.createElement("div");
+    pixel.addEventListener("mousemove", (e) => {
+      e.target.setAttribute("style", "background: black;")
+    })
+    grid.appendChild(pixel);
+  }
 }
+
+drawCanvas();
