dc372827713520cff879e4e6fb2c2d8a42f37406
NeoDalmatian <aviskovic100@gmail.com>
2022-07-25 20:26:50 +0200

    implement grid system for <div>grid and simplifie for loop

--- js/app.js
+++ js/app.js
@@ -2,9 +2,8 @@ const grid = document.querySelector(".grid");
 
 for (let i = 0; i < 256; i++) {
   const pixel = document.createElement("div");
-  pixel.classList.add("pixels");
   pixel.addEventListener("mousemove", (e) => {
-    e.target.classList.add("hoverPixels");
+    e.target.setAttribute("style", "background: black;")
   })
   grid.appendChild(pixel);
 }
