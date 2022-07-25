27af676ca31f3a15ea1c7a5aef56323d65731cc0
NeoDalmatian <aviskovic100@gmail.com>
2022-07-25 14:03:47 +0200

    move eventListener in for loop

--- js/app.js
+++ js/app.js
@@ -3,13 +3,8 @@ const grid = document.querySelector(".grid");
 for (let i = 0; i < 256; i++) {
   const pixel = document.createElement("div");
   pixel.classList.add("pixels");
+  pixel.addEventListener("mousemove", (e) => {
+    e.target.classList.add("hoverPixels");
+  })
   grid.appendChild(pixel);
 }
-
-const pixels = document.querySelectorAll(".pixels");
-
-pixels.forEach((pixel) => {
-  pixel.addEventListener("mousemove", (e) => {
-      e.target.classList.add("hoverPixels");
-  })
-})
