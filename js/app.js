8caae03a63d80584979d30c02b244d3567634503
NeoDalmatian <aviskovic100@gmail.com>
2022-07-26 10:43:27 +0200

    Iplement RAINBOW button

--- js/app.js
+++ js/app.js
@@ -1,8 +1,18 @@
 const grid = document.querySelector(".grid");
-const button = document.querySelector("button");
+const button = document.querySelector(".canvas");
+const rainbow = document.querySelector(".rainbow");
 
 let canvasSize = 256;
 let n = 16;
+let rainbowVar = false;
+
+rainbow.addEventListener("click", () => {
+  rainbowVar = true;
+})
+
+function rainbowMaker () {
+  return Math.floor(Math.random() * 255);
+}
 
 button.addEventListener("click", () => {
   n = parseInt(prompt("Set canvas size (1-100)"));
@@ -16,8 +26,10 @@ button.addEventListener("click", () => {
 function drawCanvas() {
   for (let i = 0; i < canvasSize; i++) {
     const pixel = document.createElement("div");
-    pixel.addEventListener("mousemove", (e) => {
-      e.target.setAttribute("style", "background: black;")
+    pixel.addEventListener("mouseenter", (e) => {
+      if (rainbowVar) {
+        e.target.setAttribute("style", `background: rgb(${rainbowMaker()}, ${rainbowMaker()}, ${rainbowMaker()});`);
+      } else {e.target.setAttribute("style", "background: black;")}
     })
     grid.appendChild(pixel);
   }
